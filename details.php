084d1326202fcdc7bbd3652ed20b2029707af524
Milottty <milotymeri1@gmail.com>
2026-01-07 03:42:54 +0100

    comit

---
+++ details.php
@@ -0,0 +1,14 @@
+
+
+<main style="padding:30px;">
+
+<h1>Details Page</h1>
+
+<p>You clicked on card ?></p>
+
+<a href="<?php echo site_url('/'); ?>">⬅ Back to Home</a>
+
+</main>
+
+<?php
+get_footer();
--- front-page.php
+++ front-page.php
@@ -1,13 +1,14 @@
 <?php get_header(); ?>
 
-<main>
 
+
+<main>
     <section class="hero">
 
         <div class="hero-overlay"></div>
 
-        <img class="hero-banner" 
-             src="<?php echo esc_url(get_theme_file_uri('images/banner.webp')); ?>" 
+        <img class="hero-banner"
+             src="<?php echo esc_url( get_theme_file_uri('images/banner.webp') ); ?>"
              alt="Event Banner">
 
         <div class="hero-content">
@@ -21,7 +22,71 @@
         </div>
 
     </section>
+    <?php
+$cards = [
+    [
+        "title" => "BridgeWorx",
+        "image" => "img1.jpeg",
+        "url"   => "https://bridgeworx.org.au/"
+    ],
+    [
+        "title" => "Prompt Labs",
+        "image" => "img2.jpg",
+        "url"   => "https://www.prompthealth.com/pps/2025"
+    ],
+    [
+        "title" => "Dual Studio",
+        "image" => "img3.png",
+        "url"   => "https://www.funparquesaojoao.pt/"
+    ],
+    [
+        "title" => "CHD ART MAKER",
+        "image" => "img4.png",
+        "url"   => "https://www.chdartmaker.com/"
+    ],
+    [
+        "title" => "Monuma",
+        "image" => "img5.jpg",
+        "url"   => "https://monuma.ca/"
+    ],[
+        "title" => "Austo Entertainment",
+        "image" => "img6.png",
+        "url"   => "https://www.austoentertainment.com/"
+    ],[
+        "title" => "Harlem Stage",
+        "image" => "img7.jpg",
+        "url"   => "https://www.harlemstage.org/"
+    ],[
+        "title" => "Sin Expo 2025",
+        "image" => "img8.png",
+        "url"   => "https://www.expo.sinstudiogallery.com/"
+    ],
+];
+?>
+
+<div class="grid">
+
+<?php foreach( $cards as $c ): ?>
+    
+    <a class="card"
+       href="<?php echo esc_url( $c['url'] ); ?>"
+       target="_blank"
+       rel="noopener">
+
+        <img src="<?php echo esc_url( get_theme_file_uri( 'images/' . $c['image'] ) ); ?>"
+             alt="<?php echo esc_attr( $c['title'] ); ?>">
+
+        <div class="content">
+            <?php echo esc_html( $c['title'] ); ?>
+        </div>
+
+    </a>
+
+<?php endforeach; ?>
+
+</div>
 
 </main>
+
 
 <?php get_footer(); ?>
